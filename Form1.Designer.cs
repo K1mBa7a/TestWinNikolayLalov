3ed946b578d6dd009c398d8fd0ff3e7c3cf01b74
Cecoo2004 <107184067+Cecoo2004@users.noreply.github.com>
2023-02-23 13:24:49 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace TestWinNikolayLalov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Frstn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Scndn = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.Label();
+            this.Joblabel = new System.Windows.Forms.Label();
+            this.Lstn = new System.Windows.Forms.TextBox();
+            this.Idtext = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.OwnerOfaCompanyButton = new System.Windows.Forms.RadioButton();
+            this.WorklessButton = new System.Windows.Forms.RadioButton();
+            this.ReadyButton = new System.Windows.Forms.Button();
+            this.MaleOrFemale = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Frstn
+            // 
+            this.Frstn.Location = new System.Drawing.Point(102, 114);
+            this.Frstn.Multiline = true;
+            this.Frstn.Name = "Frstn";
+            this.Frstn.Size = new System.Drawing.Size(151, 42);
+            this.Frstn.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(120, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(265, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Second Name";
+            // 
+            // Scndn
+            // 
+            this.Scndn.Location = new System.Drawing.Point(259, 114);
+            this.Scndn.Multiline = true;
+            this.Scndn.Name = "Scndn";
+            this.Scndn.Size = new System.Drawing.Size(168, 42);
+            this.Scndn.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(448, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Name";
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.ID.Location = new System.Drawing.Point(155, 238);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(33, 25);
+            this.ID.TabIndex = 5;
+            this.ID.Text = "ID";
+            // 
+            // Joblabel
+            // 
+            this.Joblabel.AutoSize = true;
+            this.Joblabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.Joblabel.Location = new System.Drawing.Point(316, 238);
+            this.Joblabel.Name = "Joblabel";
+            this.Joblabel.Size = new System.Drawing.Size(55, 25);
+            this.Joblabel.TabIndex = 6;
+            this.Joblabel.Text = "JOB";
+            // 
+            // Lstn
+            // 
+            this.Lstn.Location = new System.Drawing.Point(433, 114);
+            this.Lstn.Multiline = true;
+            this.Lstn.Name = "Lstn";
+            this.Lstn.Size = new System.Drawing.Size(146, 42);
+            this.Lstn.TabIndex = 7;
+            // 
+            // Idtext
+            // 
+            this.Idtext.Location = new System.Drawing.Point(102, 266);
+            this.Idtext.Multiline = true;
+            this.Idtext.Name = "Idtext";
+            this.Idtext.Size = new System.Drawing.Size(151, 42);
+            this.Idtext.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(259, 266);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 42);
+            this.textBox1.TabIndex = 9;
+            // 
+            // OwnerOfaCompanyButton
+            // 
+            this.OwnerOfaCompanyButton.AutoSize = true;
+            this.OwnerOfaCompanyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.OwnerOfaCompanyButton.Location = new System.Drawing.Point(259, 314);
+            this.OwnerOfaCompanyButton.Name = "OwnerOfaCompanyButton";
+            this.OwnerOfaCompanyButton.Size = new System.Drawing.Size(161, 29);
+            this.OwnerOfaCompanyButton.TabIndex = 12;
+            this.OwnerOfaCompanyButton.TabStop = true;
+            this.OwnerOfaCompanyButton.Text = "SelfEmployer";
+            this.OwnerOfaCompanyButton.UseVisualStyleBackColor = true;
+            // 
+            // WorklessButton
+            // 
+            this.WorklessButton.AutoSize = true;
+            this.WorklessButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.WorklessButton.Location = new System.Drawing.Point(259, 349);
+            this.WorklessButton.Name = "WorklessButton";
+            this.WorklessButton.Size = new System.Drawing.Size(127, 29);
+            this.WorklessButton.TabIndex = 13;
+            this.WorklessButton.TabStop = true;
+            this.WorklessButton.Text = "WorklesS";
+            this.WorklessButton.UseVisualStyleBackColor = true;
+            // 
+            // ReadyButton
+            // 
+            this.ReadyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.ReadyButton.Location = new System.Drawing.Point(321, 486);
+            this.ReadyButton.Name = "ReadyButton";
+            this.ReadyButton.Size = new System.Drawing.Size(161, 58);
+            this.ReadyButton.TabIndex = 14;
+            this.ReadyButton.Text = "Ready";
+            this.ReadyButton.UseVisualStyleBackColor = true;
+            this.ReadyButton.Click += new System.EventHandler(this.ReadyButton_Click);
+            // 
+            // MaleOrFemale
+            // 
+            this.MaleOrFemale.AutoSize = true;
+            this.MaleOrFemale.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.MaleOrFemale.Location = new System.Drawing.Point(160, 172);
+            this.MaleOrFemale.Name = "MaleOrFemale";
+            this.MaleOrFemale.Size = new System.Drawing.Size(81, 54);
+            this.MaleOrFemale.TabIndex = 15;
+            this.MaleOrFemale.Text = "Male\r\n\r\n";
+            this.MaleOrFemale.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(241, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 16);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(569, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 16);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(415, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 16);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "*";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(377, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(12, 16);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "*";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Location = new System.Drawing.Point(194, 238);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(12, 16);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1083, 556);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.MaleOrFemale);
+            this.Controls.Add(this.ReadyButton);
+            this.Controls.Add(this.WorklessButton);
+            this.Controls.Add(this.OwnerOfaCompanyButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Idtext);
+            this.Controls.Add(this.Lstn);
+            this.Controls.Add(this.Joblabel);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Scndn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Frstn);
+            this.Name = "Form1";
+            this.Text = "SiteRegister";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Frstn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Scndn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label Joblabel;
+        private System.Windows.Forms.TextBox Lstn;
+        private System.Windows.Forms.TextBox Idtext;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton OwnerOfaCompanyButton;
+        private System.Windows.Forms.RadioButton WorklessButton;
+        private System.Windows.Forms.Button ReadyButton;
+        private System.Windows.Forms.CheckBox MaleOrFemale;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWinNikolayLalov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadyButton_Click(object sender, EventArgs e)
+        {
+            if (Frstn.Text == " ")
+            {
+                MessageBox.Show("You do not fill the empy side for the First Name");
+            }
+            if (Scndn.Text == " ")
+            {
+                MessageBox.Show("You do not fill the empy side for the Second Name");
+            }
+            if (Lstn.Text == " ")
+            {
+                MessageBox.Show("You do not fill the empy side for the Last Name");
+            }
+            if (ID.Text == " ")
+            {
+                MessageBox.Show("You do not fill the empy side for the ID");
+            }
+            if (textBox1.Text == " ")
+            {
+                MessageBox.Show("You do not fill the empy side for the job");
+            }
+            if (ID.Text.Length==10)
+            {
+                MessageBox.Show("ok 10 symbols");
+            }
+            if (ID.Text.Length >= 10&&ID.Text.Length<=10)
+            {
+                MessageBox.Show("cant be lower or higher than 10");
+            }
+            if (MaleOrFemale.Checked)
+            {
+                MessageBox.Show("male");
+
+            }
+            else
+            {
+                MessageBox.Show("female");
+            }
+
+            DialogResult RegisterInfo = MessageBox.Show($"your name {Frstn.Text}{Scndn.Text}{Lstn.Text}");
+            if (RegisterInfo==DialogResult.OK)
+            {
+                Frstn.Text = "";
+                Scndn.Text = "";
+                Lstn.Text= " ";
+
+            
+            }
+        }
+    }
+}
